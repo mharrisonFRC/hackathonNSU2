970593691496951e499daa9a4989ae9d17c11262
Matt Harrison <matthew.harrsion@forrent.com>
2014-02-06 14:40:41 -0500

    added case for no GET data.

--- reg_hack.php
+++ reg_hack.php
@@ -1,23 +1,22 @@
 <?php
-
 //When the submit button is pressed on html page
-   if($_GET['fname']){
-      //Declares variables store strings 
-      $uname = $_GET['uname'];
-      $pword = $_GET['pword'];
-      $pword2 = $_GET['pword2'];
-      $fname = $_GET['fname'];
-      $lname = $_GET['lname'];
-      $email = $_GET['email'];
-      
-   }
-      
+if(isset($_GET['fname'])){
+  //Declares variables store strings 
+  $uname = $_GET['uname'];
+  $pword = $_GET['pword'];
+  $pword2 = $_GET['pword2'];
+  $fname = $_GET['fname'];
+  $lname = $_GET['lname'];
+  $email = $_GET['email'];
+}
 ?>
-      <p>uname: <?= $uname; ?></p>
-      <p>pword: <?= $pword; ?></p>
-      <p>pword2: <?= $pword2; ?></p>
-      <p>fname: <?= $fname; ?></p>
-      <p>lname: <?= $lname; ?></p>
-      <p>email: <?= $email; ?></p>
-      
-      
+<?php if(isset($fname)){ ?>
+	<p>uname: <?= $uname; ?></p>
+	<p>pword: <?= $pword; ?></p>
+	<p>pword2: <?= $pword2; ?></p>
+	<p>fname: <?= $fname; ?></p>
+	<p>lname: <?= $lname; ?></p>
+	<p>email: <?= $email; ?></p>
+<?php } else { ?>
+	<p>you are not registered.</p>
+<?php } ?>
