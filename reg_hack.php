e01ddd18f2248344f663c85929f7bbd77c090fa2
Aaron Mcfall <aaron.mcfall@yahoo.com>
2014-02-06 17:40:40 -0500

    Working on DB connection

--- reg_hack.php
+++ reg_hack.php
@@ -10,6 +10,10 @@ if(isset($_GET['fname'])){
   $email = $_GET['email'];
 }
 ?>
+
+
+
+
 <?php if(isset($fname)){ ?>
 	<p>uname: <?= $uname; ?></p>
 	<p>pword: <?= $pword; ?></p>
@@ -20,3 +24,40 @@ if(isset($_GET['fname'])){
 <?php } else { ?>
 	<p>you are not registered.</p>
 <?php } ?>
+
+
+
+
+
+
+
+
+<?php
+/* */
+ //Stores server information into variables
+    $server = 'localhost';
+    $db_username = 'root';
+    $db_password = 'root';
+    $database = 'hacku';
+
+   ///Php conects to mysql database 
+    $connection=mysql_connect($server, $db_username, $db_password)
+        or die('Could not connect to the mySQL databse');
+         
+    mysql_select_db($database, $connection)
+       or die('could not connect to the database');
+       
+       $sql = "SELECT * FROM user"; 
+       mysql_select_db("hacku", $con);
+       $myData = mysql_query($sql, $con);
+       echo 'myData: ' . mysql_num_rows($myData);
+
+       while($record = mysql_fetch_array ($myData)){
+       		echo $record['fname'];
+       }
+       mysql_close($con)
+       
+   
+
+ 
+    ?>
